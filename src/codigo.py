ae4727e15e29ae407773a1089d2a41f1afeb6a2d
rafaracar <72815786+rafaracar@users.noreply.github.com>
2020-12-01 13:22:12 +0100

    Add files via upload
    sistema de filtrado hecho

--- src/codigo.py
+++ src/codigo.py
@@ -7,95 +7,10 @@ Created on 10 nov. 2020
 import csv
 from builtins import set
 from _ast import If, Or
+from Pokemon import  Pokemon
 contador=0 
 list =[]
 
-class Pokemon:
-    num_pokedex = ""
-    nombre =  ""
-    nombre_japones =  ""
-    tipo1 = ""
-    tipo2 = ""
-    habilidad =  ""
-    altura = ""
-    peso = ""
-    contra_bicho =  ""
-    contra_siniestro =  ""
-    contra_dragon =  ""
-    contra_electrico =  ""
-    contra_hada =  ""
-    contra_acero =  ""
-    contra_lucha =  ""
-    contra_fuego =  ""
-    contra_volador =  ""
-    contra_fantasma =  "" 
-    contra_planta =  ""
-    contra_tierra =  ""
-    contra_hielo =  ""
-    contra_normal =  ""
-    contra_veneno =   ""
-    contra_psiquico =  ""
-    contra_roca =  ""
-    contra_agua =  ""
-    exp_nivel =  ""
-    ataque =  ""
-    ataque_especial =  ""
-    defensa =  ""
-    defensa_especial =  ""
-    vida =  ""
-    velocidad =  ""
-    pasos_huevo =  ""
-    ratio_captura =  ""
-    felicidad_base =  ""
-    legendario =   ""
-    total_estadisticas_base =  ""
-    porcentaje_macho = ""
-    clasificacion = ""
-    generacion = ""
-    
-    
-    def __init__(self, habilidad,contra_bicho,contra_siniestro,contra_dragon,contra_electrico,contra_hada,contra_lucha,contra_fuego,contra_volador,contra_fantasma,contra_planta,contra_tierra,contra_hielo,contra_normal,contra_veneno,contra_psiquico,contra_roca,contra_acero,contra_agua,ataque,pasos_huevo,felicidad_base,total_estadisticas_base,ratio_captura,clasificacion,defensa,exp_nivel,altura,vida,nombre_japones,nombre,porcentaje_macho,num_pokedex,ataque_especial,defensa_especial,velocidad,tipo1,tipo2,peso,generacion,legendario):
-        self.num_pokedex = num_pokedex
-        self.nombre = nombre
-        self.tipo1 = tipo1
-        self.tipo2 = tipo2
-        self.habilidad = habilidad
-        self.altura = altura
-        self.peso = peso
-        self.contra_bicho = contra_bicho
-        self.contra_siniestro = contra_siniestro
-        self.contra_dragon = contra_dragon
-        self.contra_electrico = contra_electrico
-        self.contra_hada =  contra_hada
-        self.contra_acero =  contra_acero
-        self.contra_lucha =  contra_lucha
-        self.contra_fuego =  contra_fuego
-        self.contra_volador =  contra_volador
-        self.contra_fantasma =   contra_fantasma
-        self.contra_planta =  contra_planta
-        self.contra_tierra =  contra_tierra
-        self.contra_hielo =  contra_hielo
-        self.contra_normal =  contra_normal
-        self.contra_veneno =   contra_veneno
-        self.contra_psiquico =  contra_psiquico
-        self.contra_roca =  contra_roca
-        self.contra_agua =  contra_agua
-        self.exp_nivel =  exp_nivel
-        self.ataque =  ataque
-        self.ataque_especial =  ataque_especial
-        self.defensa =  defensa
-        self.defensa_especial =  defensa_especial
-        self.vida =  vida
-        self.velocidad =  velocidad
-        self.pasos_huevo =  pasos_huevo
-        self.ratio_captura =  ratio_captura
-        self.felicidad_base =  felicidad_base
-        self.legendario =   legendario
-        self.total_estadisticas_base =  total_estadisticas_base
-        self.porcentaje_macho = porcentaje_macho
-        self.clasificacion = clasificacion
-        self.generacion = generacion
-        
 
 with open("../pokemon.csv", encoding="utf8") as f:
     reader= csv.reader(f)
@@ -112,45 +27,125 @@ print("¿Desea realizar una busqueda en la base de datos?")
 
 
 
-primer_input= input("Escriba 'Si' o 'No':")
-def filtra_tipos (pokemon):
-    return pokemon.tipo1 or pokemon.tipo2 == tipo_para_buscar #no se hacer un sistema de filtrado
-if primer_input == 'Si':
+primer_input= input("Escriba 'si' o 'no':")
+def filtra_tipos ():
+    with open("../pokemon.csv", encoding="utf8") as f: 
+        tipo_para_buscar= input("Escribe el tipo por el que va a filtrar:")
+        reader= csv.reader(f)
+        for row in reader:
+            if tipo_para_buscar in row[35] or tipo_para_buscar in row[36]:
+                print("Su nombre es", row[30], ",y sus tipos son", row[36],"y", row[37])
+def buscar_por_nombre (): 
+        nombre_a_buscar = (input("escribe el nombre del pokemon a buscar:"))
+        for pokemon in list:
+            if pokemon.nombre.lower() == nombre_a_buscar.lower():
+                print("Su numero de la pokedex es:", pokemon.num_pokedex, ".Su nombre es:",pokemon.nombre, ".Sus habilidades pueden ser:", pokemon.habilidad,".Su tipo es:", pokemon.tipo1, pokemon.tipo2, ".Su altura es:", pokemon.altura,"m", ".Su peso es:", pokemon.peso,"Kg")
+
+def buscar_por_num_pokedex ():
+        numero_pokedex= (input("escbribe un numero de la pokedex -del 1 al 801-"))
+        for pokemon in list:
+            if pokemon.num_pokedex == numero_pokedex:
+                    print("Su numero de la pokedex es:", pokemon.num_pokedex, ".Su nombre es:",pokemon.nombre, ".Sus habilidades pueden ser:", pokemon.habilidad,".Su tipo es:", pokemon.tipo1, pokemon.tipo2, ".Su altura es:", pokemon.altura,"m", ".Su peso es:", pokemon.peso,"Kg")
+
+
+def ver_todos_los_pokemon ():
+    for pokemon in list:
+            print(pokemon.nombre, "Tipo:", pokemon.tipo1, pokemon.tipo2, "Su ratio de captura es:", pokemon.ratio_captura, "la curva de experiencia es:", pokemon.exp_nivel, "El porcentaje de que te encuentres a un macho en estado salvaje es:", pokemon.porcentaje_macho, "Es legendario:", pokemon.legendario)
+
+
+def tabla_debilidad_fuerza ():
+     nombre_a_buscar= input("Escribe el nombre del pokemon del que quieres saber sus debilidades,")
+     for pokemon in list:
+            if pokemon.nombre.lower()== nombre_a_buscar.lower():
+                print("Contra acero", pokemon.contra_acero,"Contra agua", pokemon.contra_agua,"Contra bicho", pokemon.contra_bicho,"Contra dragon", pokemon.contra_dragon,"Contra electrico", pokemon.contra_electrico,"Contra Fantasma", pokemon.contra_fantasma,"Contra Fuego", pokemon.contra_fuego,"Contra Hada", pokemon.contra_hada,"Contra hielo", pokemon.contra_hielo,"Contra lucha", pokemon.contra_lucha,"Contra normal", pokemon.contra_normal,"Contra planta", pokemon.contra_planta,"Contra psiquico", pokemon.contra_psiquico,"Contra roca", pokemon.contra_roca,"Contra siniestro", pokemon.contra_siniestro,"Contra tierra", pokemon.contra_tierra,"Contra veneno", pokemon.contra_veneno,"Contra volador", pokemon.contra_volador)
+if primer_input == 'si':
     print("""¿Como desea buscar?
     1- Por nombre
     2- Por numero de pokedex
     3- Por tipo
     4- Quiero ver todos los Pokemon
-    5- Quiero saber a que es fuerte y debil un pokemon """)
+    5- Quiero saber a que es fuerte y debil un pokemon 
+    6- proximamente
+    7- proximamente
+    8- proximamente""")
+
     
     segundo_input= input()    
     if segundo_input== '1':
-        nombre_a_buscar = (input("escribe el nombre del pokemon a buscar:"))
-        for pokemon in list:
-            if pokemon.nombre.lower() == nombre_a_buscar.lower():
-                print("Su numero de la pokedex es:", pokemon.num_pokedex, ".Su nombre es:",pokemon.nombre, ".Sus habilidades pueden ser:", pokemon.habilidad,".Su tipo es:", pokemon.tipo1, pokemon.tipo2, ".Su altura es:", pokemon.altura,"m", ".Su peso es:", pokemon.peso,"Kg")
+        buscar_por_nombre()
     elif segundo_input== '2':
-        numero_pokedex= (input("escbribe un numero de la pokedex -del 1 al 801-"))
-        for pokemon in list:
-            if pokemon.num_pokedex == numero_pokedex:
-                    print("Su numero de la pokedex es:", pokemon.num_pokedex, ".Su nombre es:",pokemon.nombre, ".Sus habilidades pueden ser:", pokemon.habilidad,".Su tipo es:", pokemon.tipo1, pokemon.tipo2, ".Su altura es:", pokemon.altura,"m", ".Su peso es:", pokemon.peso,"Kg")
+        buscar_por_num_pokedex()
     elif segundo_input== '3':
-        tipo_para_buscar= input("Escribe el tipo por el que va a filtrar:")
-        for pokemon in list:
-            if pokemon.tipo1 or pokemon.tipo2 == tipo_para_buscar:
-                print(pokemon.nombre)
-                
-    elif segundo_input== '3':
-        for pokemon in list:
-            print("Su numero de la pokedex es:", pokemon.num_pokedex, ".Su nombre es:",pokemon.nombre, ".Sus habilidades pueden ser:", pokemon.habilidad,".Su tipo es:", pokemon.tipo1, pokemon.tipo2, ".Su altura es:", pokemon.altura,"m", ".Su peso es:", pokemon.peso,"Kg")
+        filtra_tipos()          
     elif segundo_input== '4':
-        for pokemon in list:
-           print(pokemon.nombre, "Tipo:", pokemon.tipo1, pokemon.tipo2, "Su ratio de captura es:", pokemon.ratio_captura, "la curva de experiencia es:", pokemon.exp_nivel, "El porcentaje de que te encuentres a un macho en estado salvaje es:", pokemon.porcentaje_macho, "Es legendario:", pokemon.legendario )
+        ver_todos_los_pokemon()
     elif segundo_input== '5':
-        nombre_a_buscar= input("Escribe el nombre del pokemon del que quieres saber sus debilidades,")
-        for pokemon in list:
-            if pokemon.nombre.lower()== nombre_a_buscar.lower():
-                print("Contra acero", pokemon.contra_acero,"Contra agua", pokemon.contra_agua,"Contra bicho", pokemon.contra_bicho,"Contra dragon", pokemon.contra_dragon,"Contra electrico", pokemon.contra_electrico,"Contra Fantasma", pokemon.contra_fantasma,"Contra Fuego", pokemon.contra_fuego,"Contra Hada", pokemon.contra_hada,"Contra hielo", pokemon.contra_hielo,"Contra lucha", pokemon.contra_lucha,"Contra normal", pokemon.contra_normal,"Contra planta", pokemon.contra_planta,"Contra psiquico", pokemon.contra_psiquico,"Contra roca", pokemon.contra_roca,"Contra siniestro", pokemon.contra_siniestro,"Contra tierra", pokemon.contra_tierra,"Contra veneno", pokemon.contra_veneno,"Contra volador", pokemon.contra_volador)
+        tabla_debilidad_fuerza()     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+elif primer_input == 'no':
+    print("Vale, no quieres hacer nada, te entiendo.")
+        
+        
+       
+        
+       
+        
+        
+        
         
         
         
